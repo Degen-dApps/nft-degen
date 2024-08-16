4ae9e650e0a05bfa9097685473caa90f0fdc8819
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2024-08-16 17:14:07 +0200

    video NFTs link added to dropdown

--- components/nft/list/NftListDropdown.vue
+++ components/nft/list/NftListDropdown.vue
@@ -5,6 +5,7 @@
     </button>
     <ul class="dropdown-menu">
       <li><NuxtLink class="dropdown-item" to="/nft/music-nfts"><i class="bi bi-music-note-beamed me-2"></i>Music NFTs</NuxtLink></li>
+      <li><NuxtLink class="dropdown-item" to="/nft/video-nfts"><i class="bi bi-camera-reels me-2"></i>Video NFTs</NuxtLink></li>
       <li><NuxtLink class="dropdown-item" to="/nft/featured"><i class="bi bi-patch-check me-2"></i>Featured NFTs</NuxtLink></li>
       <li><NuxtLink class="dropdown-item" to="/nft"><i class="bi bi-stars me-2"></i>New NFTs</NuxtLink></li>
       <li><NuxtLink class="dropdown-item" to="/nft/most-traded"><i class="bi bi-arrow-left-right me-2"></i>Most Traded NFTs</NuxtLink></li>
